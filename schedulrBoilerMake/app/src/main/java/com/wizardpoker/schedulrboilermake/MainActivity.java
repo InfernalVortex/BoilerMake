ede73ef834ae0d496e88877ae01e9ac9101821cb
jmpoholarz <jmpoholarz@gmail.com>
2015-10-17 16:22:39 -0400

    Updated var names in mainActivity

--- schedulrBoilerMake/app/src/main/java/com/wizardpoker/schedulrboilermake/MainActivity.java
+++ schedulrBoilerMake/app/src/main/java/com/wizardpoker/schedulrboilermake/MainActivity.java
@@ -12,19 +12,20 @@ public class MainActivity extends Activity {
     public static final String PREFS_NAME = "MyPrefsFile";
 
     private boolean mUnderPressure;
-    private int mBreakInterval;
-    private int mBreakPeriod;
+    private boolean mBreakDuring;
+    private int mBreakFrequency;
+
 
     public void setUnderPressure(boolean mUnderPressure) {
         this.mUnderPressure = mUnderPressure;
     }
 
-    public void setBreakInterval(int mBreakInterval) {
-        this.mBreakInterval = mBreakInterval;
+    public void setBreakFrequency(int mBreakFrequency) {
+        this.mBreakFrequency = mBreakFrequency;
     }
 
-    public void setBreakPeriod(int mBreakPeriod) {
-        this.mBreakPeriod = mBreakPeriod;
+    public void setBreakPeriod(boolean mBreakDuring) {
+        this.mBreakDuring = mBreakDuring;
     }
 
     @Override
@@ -37,11 +38,11 @@ public class MainActivity extends Activity {
         // Restore Data from previous opening if exists
         SharedPreferences settings = getSharedPreferences(PREFS_NAME, 0);
         boolean underPressure = settings.getBoolean("underPressure", false);
-        int breakInterval = settings.getInt("breakInterval", 15);
-        int breakPeriod = settings.getInt("breakPeriod", 5);
+        int breakFrequency = settings.getInt("breakFrequency", 1); // default 1 = med (30:10)
+        boolean breakDuring = settings.getBoolean("breakDuring", true);
         setUnderPressure(underPressure);
-        setBreakInterval(breakInterval);
-        setBreakPeriod(breakPeriod);
+        setBreakFrequency(breakFrequency);
+        setBreakPeriod(breakDuring);
     }
 
 
@@ -76,8 +77,9 @@ public class MainActivity extends Activity {
         SharedPreferences settings = getSharedPreferences(PREFS_NAME, 0);
         SharedPreferences.Editor editor = settings.edit();
         editor.putBoolean("underPressure", mUnderPressure);
-        editor.putInt("breakInterval", mBreakInterval);
-        editor.putInt("breakPeriod", mBreakPeriod);
+        editor.putBoolean("breakPeriod", mBreakDuring);
+        editor.putInt("breakFrequency", mBreakFrequency);
+
 
         //Commit edits
         editor.commit();
