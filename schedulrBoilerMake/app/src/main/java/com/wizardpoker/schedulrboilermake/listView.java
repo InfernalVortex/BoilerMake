5a81ac55221cd5a794c7671be1996feb8c4a813b
InfernalVortex <pranz10@gmail.com>
2015-10-17 23:04:49 -0400

    I fix it

--- schedulrBoilerMake/app/src/main/java/com/wizardpoker/schedulrboilermake/listView.java
+++ schedulrBoilerMake/app/src/main/java/com/wizardpoker/schedulrboilermake/listView.java
@@ -17,7 +17,8 @@ public class listView extends Activity {
 
     protected void onCreate(Bundle savedInstanceStates){
         super.onCreate(savedInstanceStates);
-
+        setContentView(R.layout.activity_listview);
+        
         Schedule s = new Schedule(true, true, 1, true);
         ListAdapter adapter = new ArrayAdapter<String>(this,
         android.R.layout.simple_list_item_1, android.R.id.text1, s.refreshSchedule());
@@ -25,6 +26,5 @@ public class listView extends Activity {
         ListView myListView = (ListView) findViewById(R.id.listView);
         myListView.setAdapter(adapter);
 
-        setContentView(R.layout.activity_listview);
     }
 }
