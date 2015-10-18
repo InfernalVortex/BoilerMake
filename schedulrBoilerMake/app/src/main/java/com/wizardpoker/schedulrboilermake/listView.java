8359ee838b25d183dd45f7bd228ce3c667b5ae2d
InfernalVortex <pranz10@gmail.com>
2015-10-17 22:08:55 -0400

    There are now a schedule for you

---
+++ schedulrBoilerMake/app/src/main/java/com/wizardpoker/schedulrboilermake/listView.java
@@ -0,0 +1,17 @@
+package com.wizardpoker.schedulrboilermake;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+/**
+ * Created by Pranav on 10/17/2015.
+ */
+public class listView extends Activity {
+    protected void onCreate(Bundle savedInstanceStates){
+        super.onCreate(savedInstanceStates);
+        setContentView(R.layout.activity_listview);
+
+    }
+}
